43fecc2ec200316b9888fa07d7f606fd580d032b
funnybones69 <funnybones69@users.noreply.github.com>
2026-08-07 17:45:02 +0300

    localtun: supervise runtime and preserve teardown invariants

--- internal/localtun/route.go
+++ internal/localtun/route.go
@@ -6,3 +6,11 @@ type routeController interface {
 	Setup(context.Context) error
 	Cleanup(context.Context) error
 }
+
+type supervisedRouteController interface {
+	routeController
+	DNSDone() <-chan error
+	DNSError() error
+	Health(context.Context) error
+	EnterFailClosed(context.Context) error
+}
